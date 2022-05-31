1ef77c831bc41879b55212c2e206362ca36258f8
Shrish Kerur <100413262+theshrish46@users.noreply.github.com>
2022-05-31 14:48:10 +0530

    Update circlr.c

--- circlr.c
+++ circlr.c
@@ -1,5 +1,20 @@
-#include<stdio.h>
+#include <stdio.h>
 int main(){
-printf("Hello world\n");
-return 0;
+  // Initializing the Radius and valuse of PI.
+  int radius;
+  float pi = 3.14;
+  // Initializing the variables for Area and Circumference.
+  float area, circumference;
+  
+  //Accepting the value of radius.
+  printf("Enter the radius of the circle : \n");
+  scanf("%d", &radius);
+  
+  //Calculating the are and circumference of the circle usinf the formulas: area = 2*⨅*radius and circumference = ⨅*radius*radius.
+  area = 2 * PI * radius;
+  circumference = PI * radius * radius;
+  
+  //Printing the value of area and circumference.
+  printf("Area of the circle is %d  and  Circumference of the circle is %d\n", area, circumference);
+  return 0;
 }
